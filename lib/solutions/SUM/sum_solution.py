b0ca602f39306741aa4ebe0f41599d3808870344
sbx_user1051 <sbx_user1051@169.254.0.85>
2020-12-10 20:09:00 +0000

    Thu Dec 10 20:09:00 UTC 2020

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,11 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    raise NotImplementedError()
+    if type(x) == int and type(y) == int:
+        if x > 0  and x < 100  and y > 0 and y < 100:
+            return x+y
+        else:
+            print("Please provide value between 0 to 100  ")
+    else:
+        print("Please provide interger only")
+
+print(compute(2,5))
