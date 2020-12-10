fe85d15bc177ec893f6214a9d00adc5110baf45d
sbx_user1051 <sbx_user1051@169.254.0.85>
2020-12-10 20:21:37 +0000

    Thu Dec 10 20:21:37 UTC 2020

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,4 +3,12 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    raise NotImplementedError()
+    if type(friend_name) == str:
+        return friend_name
+    else:
+        error  =  "Plese provide only string "
+        return error
+
+hello("hello world")
+
+
