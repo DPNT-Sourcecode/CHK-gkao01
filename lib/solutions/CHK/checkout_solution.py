7956c06d0ecc007dd3b15f43d7a1be5f763af009
sbx_user1051 <sbx_user1051@169.254.87.133>
2020-12-11 19:18:08 +0000

    Fri Dec 11 19:18:08 UTC 2020

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,18 +5,18 @@
 import re
 def checkout(skus):
     price_table ={
-        "A": {
-            "price" : 50,
-            "required_unit_for_offer" : 3,
-            "special_price" : 130 
-             },
-        "B":  {
-            "price" : 30,
-            "required_unit_for_offer" : 2,
-            "special_price" : 45 
-             },
-        "C" : 20,
-        "D": 15,
+        # "A": {
+        #     "price" : 50,
+        #     "required_unit_for_offer" : 3,
+        #     "special_price" : 130 
+        #      },
+        # "B":  {
+        #     "price" : 30,
+        #     "required_unit_for_offer" : 2,
+        #     "special_price" : 45 
+        #      },
+        # "C" : 20,
+        # "D": 15,
         
     }
 
@@ -54,3 +54,4 @@ def checkout(skus):
 
 
 print(checkout(""))
+
