5013d4934b1c24cd8de6fdb7df31dbadc7da0022
sbx_user1051 <sbx_user1051@169.254.87.133>
2020-12-11 17:31:04 +0000

    Fri Dec 11 17:31:04 UTC 2020

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,20 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    price_table ={
+        "A": 50,
+        "B": 30,
+        "C" : 20,
+        "D": 15,
+        "special_offer": {
+            "3A" : 130,
+            "2B" : 45
+        }
+    }
+
+    if type(skus) == str:
+        if len(skus) == 1:
+            return checkout[skus]
+    else:
+        return -1
+
