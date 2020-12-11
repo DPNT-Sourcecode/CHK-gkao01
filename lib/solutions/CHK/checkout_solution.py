ba9d8ff59a94e87778447dd5f88f2776eb7fd110
sbx_user1051 <sbx_user1051@169.254.87.133>
2020-12-11 18:46:05 +0000

    Fri Dec 11 18:46:05 UTC 2020

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,6 +2,7 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+import re
 def checkout(skus):
     price_table ={
         "A": {
@@ -18,7 +19,9 @@ def checkout(skus):
         "D": 15,
         
     }
+
     skus = skus.upper()
+
     if type(skus) == str and not skus.isdigit():
         if len(skus) == 1:
             if type(price_table[skus]) == int:
@@ -26,11 +29,25 @@ def checkout(skus):
             else:
                 return price_table[skus]["price"]
         else:
-            pass
-
-
+            match  = re.match(r"([0-9])([a-z]+)",skus,re.I)
+            if match:
+                item = match.group()
+                if type(price_table[item[0]]) == int:
+                    return int(item[0]) * price_table[item[1]]
+                else:
+                    required_unit = price_table[item[1]]["required_unit_for_offer"]
+                    if int(item[0]) == required_unit:
+                        return price_table[item[1]]["special_price"]
+                    else:
+                        remaining_unit = int(item[0]) % price_table[item[1]]["required_unit_for_offer"]
+                        discounted_unit = (int(item[0]) - remaining_unit) // price_table[item[1]]["required_unit_for_offer"]
+                        final_total = discounted_unit * price_table[item[1]]["special_price"] + remaining_unit * price_table[item[1]]["price"]
+                        return final_total
+            else:
+                return -1
     else:
         return -1
 
 
 print(checkout("c"))
+
