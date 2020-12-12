e3a39ddd24324a086a869f15f1d59192f30bf4ba
sbx_user1051 <sbx_user1051@169.254.151.245>
2020-12-12 01:10:23 +0000

    Sat Dec 12 01:10:23 UTC 2020

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,52 +7,57 @@ def checkout(skus):
     price_table ={
         "A": {
             "price" : 50,
+            "offer" : True
             "required_unit_for_offer" : 3,
             "special_price" : 130 
              },
         "B":  {
             "price" : 30,
+            "offer" : True
             "required_unit_for_offer" : 2,
             "special_price" : 45 
              },
-        "C" : 20,
-        "D": 15,
-        
+        "C" : {"price" : 20, "offer" : False },
+        "D": {"price" : 15, "offer" : False} 
     }
 
-    skus = skus.upper()
-
-    if type(skus) == str and not skus.isdigit():
-
-        if len(skus) == 1:
+    if len(skus) <= 1:
+        if len(skus) == 0:
+            return 0
+        else:
             try:
-                if type(price_table[skus]) == int:
-                    return price_table[skus]
-                else:
-                    return price_table[skus]["price"]
+                return price_table[skus]["price"]
             except:
                 return -1
-        else:
-            match  = re.match(r"([0-9])([a-z]+)",skus,re.I)
-            if match:
-                item = match.groups()
-                if len(item[1]) == 1:
-                    if type(price_table[item[1]]) == int:
-                        return int(item[0]) * price_table[item[1]]
-                    else:
-                        required_unit = price_table[item[1]]["required_unit_for_offer"]
-                        if int(item[0]) == required_unit:
-                            return price_table[item[1]]["special_price"]
-                        else:
-                            remaining_unit = int(item[0]) % price_table[item[1]]["required_unit_for_offer"]
-                            discounted_unit = (int(item[0]) - remaining_unit) // price_table[item[1]]["required_unit_for_offer"]
-                            final_total = discounted_unit * price_table[item[1]]["special_price"] + remaining_unit * price_table[item[1]]["price"]
-                            return final_total
-                else:
-                    return -1
-            else:
-                return -1
     else:
-        return -1
+        if skus.isupper() and skus.isalpha():
+            order_detail = list(skus)
+            total_payment = 0
+            no_of_unit = {}
+
+            for product in price_table:
+                if product in order_detail:
+                    no_of_unit[product] = order_detail.count(product)
+
+            for products, unit in no_of_unit.items():
+                if price_table[products]["offer"] == True:
+                    if price_table[products]["required_unit_for_offer"] == unit:
+                        total_payment += price_table[products]["special_price"]
+                    else:
+                        extra_unit = unit % price_table[products]["required_unit_for_offer"]
+                        pair_of_unit  = (unit - extra_unit) // price_table[products]["required_unit_for_offer"]
+                        total_price = pair_of_unit * price_table[products]["special_price"] + extra_unit * price_table[products]["price"]
+                        total_payment += total_price
+                    
+                else:
+                    total_payment += (unit * price_table[products]["price"])
+            return total_payment
+        else:
+            return -1
+        
+
+
+
 
 print(checkout("A2"))
+
